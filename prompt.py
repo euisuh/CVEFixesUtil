7cca02a23e302e5dc419e75faded752e5bd03a0e
euisuh <esj@qcri.org>
2024-05-15 09:42:21 +0300

    :sparkles: add new prompts

--- prompt.py
+++ prompt.py
@@ -1,3 +1,15 @@
+def prompt0(code):
+    return f'''Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+Please answer yes or no indicating whether the input contains any vulnerabilities.
+
+### Input:
+{code}
+
+### Response (Yes | No):
+'''
+
 def prompt1(code):
     return f'''Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
@@ -10,46 +22,53 @@ CWE-125, CWE-119, CWE-787, CWE-20, CWE-476, CWE-416, CWE-190
 ### Input:
 {code}
 
-### Response:
+### Response (Yes | No):
 '''
 
 
-def prompt2(cwe_id, cwe_name, cwe_desc, poison_code1, code):
+def prompt2(cwe_id, cwe_name, cwe_desc, code):
     return f'''Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
-### {cwe_id} {cwe_name}:
-{cwe_desc}
-
-### Code with {cwe_id}:
-{poison_code1}
-
 ### Instruction:
-Please answer yes or no whether {cwe_id} is in the following input.
+CWE-{cwe_id} {cwe_name}: {cwe_desc}
+
+Please answer yes or no whether CWE-{cwe_id} is in the following input.
 
 ### Input:
 {code}
 
-### Response:
+### Response (Yes | No):
 '''
 
 
-def prompt3(cwe_id, cwe_name, cwe_desc, poison_code1, clean_code1, code):
+def prompt21(cwe_id, cwe_name, cwe_desc, code):
     return f'''Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
-### {cwe_id} {cwe_name}:
-{cwe_desc}
-
-### Code with {cwe_id}:
-{poison_code1}
-
-### Code without {cwe_id}:
-{clean_code1}
-
 ### Instruction:
-Please answer yes or no whether {cwe_id} is in the following input.
+Please answer yes or no whether CWE-{cwe_id} is in the following input.
 
 ### Input:
 {code}
 
-### Response:
+### Response (Yes | No):
+'''
+
+
+def prompt3(cwe_id, code):
+    return f'''Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Vulnerability type: CWE-{cwe_id}
+
+### Input:
+{code}
+
+### Instruction:
+#### Define the vulnerability:
+
+#### Provide examples:
+
+#### Examine the code:
+
+#### Confirm if the input has vulnerability (Yes | No):
+
 '''
